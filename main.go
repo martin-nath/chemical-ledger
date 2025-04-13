4476274639ca80cdb38ae6079df0e1baf116f529
Shatadal Das <shatadaldas.dev@gmail.com>
2025-04-13 22:18:08 +0530

    refactor: streamline command-line flag handling for migrations and resets

--- main.go
+++ main.go
@@ -13,28 +13,31 @@ import (
 
 func main() {
 	// Parse command-line flags
-	reset := flag.Bool("reset", false, "Drop all tables before running migrations")
+	migrationFlag := *flag.Bool("migrate", false, "Run migrations")
+	resetFlag := *flag.Bool("reset", false, "Drop all tables before running migrations")
 	flag.Parse()
 
 	// Initialize the database
 	db.InitDB("./chemical_ledger.db")
 	defer db.Db.Close()
 
+	// Run migrations
+	if migrationFlag {
+		if err := migrate.CreateTables(db.Db); err != nil {
+			log.Fatalf("Migration failed: %v", err)
+		}
+
+		fmt.Println("Migration completed successfully!")
+	}
+	
 	// Drop tables if the reset flag is set
-	if *reset {
+	if resetFlag {
 		if err := migrate.DropTables(db.Db); err != nil {
 			log.Fatalf("Failed to drop tables: %v", err)
 		}
 		fmt.Println("Tables dropped successfully!")
 	}
-
-	// Run migrations
-	if err := migrate.CreateTables(db.Db); err != nil {
-		log.Fatalf("Migration failed: %v", err)
-	}
-
-	fmt.Println("Migration completed successfully!")
-
+	
 	// Set up routes
 	http.HandleFunc("/insert", handlers.InsertData) // POST /transaction
 	http.HandleFunc("/fetch", handlers.GetData)     // GET /transactions
